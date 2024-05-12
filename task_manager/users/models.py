1cea7b14cdd73966fa8dfc769a0a95ba323ba4ee
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-13 00:07:41 +0300

    Add users DB

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User as _User
 
 
-class User(AbstractUser):
+class User(_User):
     email = None
