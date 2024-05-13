1d7b1ffd272d5df4610b76fccf6eb654097b79eb
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-13 21:00:19 +0300

    Change password requirements

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -116,17 +116,11 @@ if 'DATABASE_URL' in os.environ:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa: E501
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa: E501
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa: E501
-    },
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  # noqa: E501
+        "OPTIONS": {
+            "min_length": 3,
+        },
+    }
 ]
 
 
