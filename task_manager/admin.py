e10fc6dc7162d3fda9e8f7493345e5a55c936fc3
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-16 01:04:40 +0300

    Add Status model

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from task_manager.users.models import User
+from task_manager.statuses.models import Status
 
 admin.site.register(User)
+admin.site.register(Status)
