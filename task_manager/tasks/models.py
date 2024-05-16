de5444450e964d20855fad3d9da86a49f237c5d0
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-16 19:02:38 +0300

    Add named fields for User model (fix)

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -7,8 +7,8 @@ class Task(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
-    executor = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='created_tasks')
+    executor = models.ForeignKey(User, on_delete=models.PROTECT, null=True, related_name='executing_tasks')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
