e166817b1cd7be15ae27767a008dacb53a56344a
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-16 18:52:04 +0300

    Add task model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from task_manager.users.models import User
+from task_manager.statuses.models import Status
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    executor = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
