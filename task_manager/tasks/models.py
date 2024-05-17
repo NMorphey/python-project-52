c48a323e5056de8547a0b65d14f8a215d2d92c4c
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-17 15:47:18 +0300

    Fix fields

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -5,10 +5,10 @@ from task_manager.statuses.models import Status
 
 class Task(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(null=True)
+    description = models.TextField(blank=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='created_tasks')
-    executor = models.ForeignKey(User, on_delete=models.PROTECT, null=True, related_name='executing_tasks')
+    executor = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, related_name='executing_tasks')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
