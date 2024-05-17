94ea10c8caa1e6ff83b8e61a710ed3d28c225668
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-17 22:03:38 +0300

    Fix

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from task_manager.users.models import User
 from task_manager.statuses.models import Status
 from task_manager.tasks.models import Task
-from task_manager.labels import Label
+from task_manager.labels.models import Label
 
 admin.site.register(User)
 admin.site.register(Status)
