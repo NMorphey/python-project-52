7cbcf917be88e938ac12703be61bc219f3c6165e
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-17 15:00:08 +0300

    Add success messages

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from task_manager.statuses.models import Status
 from task_manager.utils import LoginRequiredMixin
+from django.utils.translation import gettext_lazy as _
 
 
 class StatusesIndexView(LoginRequiredMixin, ListView):
@@ -11,21 +13,24 @@ class StatusesIndexView(LoginRequiredMixin, ListView):
     context_object_name = 'statuses'
 
 
-class StatusDeleteView(LoginRequiredMixin, DeleteView):
+class StatusDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Status
     template_name = 'statuses/delete.html'
     success_url = reverse_lazy('statuses_index')
+    success_message = _('The status was deleted')
 
 
-class StatusCreateView(LoginRequiredMixin, CreateView):
+class StatusCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Status
     template_name = 'statuses/create.html'
     fields = ['name']
     success_url = reverse_lazy('statuses_index')
+    success_message = _('Status created successfully')
 
 
-class StatusUpdateView(LoginRequiredMixin, UpdateView):
+class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Status
     template_name = 'statuses/update.html'
     fields = ['name']
     success_url = reverse_lazy('statuses_index')
+    success_message = _('The status was updated')
