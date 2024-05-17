09c21445f1d520af91ce5a865f1276ce74c83617
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-17 20:44:08 +0300

    Add Label model with m2m

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from task_manager.tasks.models import Task
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    tasks = models.ManyToManyField(Task, through='TaskLabelRelationship')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TaskLabelRelationship(models.Model):
+    label = models.ForeignKey(Label, on_delete=models.PROTECT)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
