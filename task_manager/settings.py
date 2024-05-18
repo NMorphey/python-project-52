1f6c372d5fcf3d840d8b47530031c15893f1fee4
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-18 16:58:16 +0300

    Fix DB config

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -124,8 +124,7 @@ DATABASES = {
 if 'DATABASE_URL' in os.environ:
     DATABASES['default'] = dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
+        conn_max_age=600
     )
 
 
